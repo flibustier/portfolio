63a7a17d0f1514df4c781c5093889165cd9dbf12
Jonathan Platteau <jplatteau.externe@m6.fr>
2020-06-13 12:19:15 +0100

    feat(deploy): add deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn generate
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/flibustier/flibustier.github.io.git master
+
+cd -
